abd3b0d74f681b154203c2c5c835e9a7fb7a9427
lubien <lubien1996@gmail.com>
2016-08-18 10:45:36 -0300

    Start material-ui

---
+++ src/containers/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import AppBar from 'material-ui/AppBar';
+
+const Navbar = () => (
+  <AppBar
+    title="Planning Poker"
+    iconClassNameRight="muidocs-icon-navigation-expand-more"
+  />
+);
+
+export default Navbar;
+
--- src/index.js
+++ src/index.js
@@ -5,6 +5,11 @@ import App from './App';
 import store from './store/';
 import './index.css';
 
+// Needed for material-ui
+// See: http://stackoverflow.com/a/34015469/988941
+import injectTapEventPlugin from 'react-tap-event-plugin';
+injectTapEventPlugin();
+
 ReactDOM.render(
   <Provider store={store}>
     <App />
