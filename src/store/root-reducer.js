57532b2d7552bea1563640f692fd735395ff8716
lubien <lubien1996@gmail.com>
2016-09-03 19:52:03 -0300

    Setup reduce-reducers

--- src/store/root-reducer.js
+++ src/store/root-reducer.js
@@ -1,11 +1,12 @@
 import {combineReducers} from 'redux-immutable';
+import reduceReducers from 'reduce-reducers';
 
 import config from '../reducers/config';
 import stories from '../reducers/stories';
 
-const rootReducer = combineReducers({
-  config, stories
-});
+const rootReducer = reduceReducers(
+  combineReducers({config, stories}),
+);
 
 export default rootReducer;
 
