a6fef00eb71cd423d4e1f987b1fdb2c3a444bc16
lubien <lubien1996@gmail.com>
2016-09-03 15:06:46 -0300

    [Closes #3] Persist state in dev

--- src/store/dev.js
+++ src/store/dev.js
@@ -1,5 +1,6 @@
 import {createStore, compose} from 'redux';
 import {fromJS} from 'immutable';
+import transit from 'transit-immutable-js';
 import rootReducer from './root-reducer';
 
 const enhancers = compose(
@@ -7,8 +8,34 @@ const enhancers = compose(
 );
 
 const store = createStore(
-    rootReducer, fromJS({}), enhancers
+    rootReducer, loadState(), enhancers
 );
 
+// Thanks to chaintan17
+// See: https://github.com/mxstbr/react-boilerplate/issues/396
+store.subscribe(function persistState() {
+  try {
+    const state = store.getState();
+    const serializedState = transit.toJSON(state);
+    localStorage.setItem('state', serializedState);
+  } catch (err) {
+    console.log('Could not save current redux state', err);
+  }
+});
+
 export default store;
+
+function loadState() {
+  try {
+    const serializedState = localStorage.getItem('state');
+
+    if (serializedState === null) {
+      return fromJS({});
+    }
+
+    return transit.fromJSON(serializedState);
+  } catch (err) {
+    return fromJS({});
+  }
+};
 
