a7aaf35260970c5615969301dcfa1a2030b81f2d
lubien <lubien1996@gmail.com>
2016-09-03 15:13:36 -0300

    [Closes #4] Remove mock data from reducers

--- src/reducers/stories.js
+++ src/reducers/stories.js
@@ -2,35 +2,7 @@ import {List, fromJS} from 'immutable';
 
 let idGenerator = 0;
 
-const INITIAL_STATE = fromJS([{
-  average: 0,
-  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
-  cards: [],
-  flipped: false,
-  id: idGenerator++,
-  position: 2
-}, {
-  average: 0,
-  description: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo',
-  cards: [],
-  flipped: false,
-  id: idGenerator++,
-  position: 1
-}, {
-  average: 0,
-  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
-  cards: [],
-  flipped: false,
-  id: idGenerator++,
-  position: 3
-}, {
-  average: 0,
-  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
-  cards: [],
-  flipped: false,
-  id: idGenerator++,
-  position: 4
-}]);
+const INITIAL_STATE = fromJS([]);
 
 export const MANIPULATE_STORY = 'app/stories/MANIPULATE_STORY';
 export const EDIT_STORY = 'app/stories/EDIT_STORY';
