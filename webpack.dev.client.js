bd3d2885680bd1e55e60d9b9d61495d83fd58606
lubien <lubien1996@gmail.com>
2016-09-09 10:57:41 -0300

    Setup webpack config files

---
+++ webpack.dev.client.js
@@ -0,0 +1,42 @@
+const webpack = require('webpack');
+const path = require('path');
+const config = require('./server/config');
+
+module.exports = {
+  entry: [
+    'react-hot-loader/patch',
+    `webpack-dev-server/client?${config.WEBPACK_BASE_URL}/`,
+    'webpack/hot/only-dev-server',
+    path.join(__dirname, './client/entry.dev.js'),
+  ],
+  output: {
+    path: path.join(__dirname, './public/scripts/'),
+    filename: 'bundle.js',
+    publicPath: `${config.WEBPACK_BASE_URL}/`,
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+        plugins: ['react-hot-loader/babel'],
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        loader: 'style!css!postcss',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx'],
+  },
+  devtool: 'source-map',
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.DefinePlugin({
+      'process.env.isClient': 'true',
+    }),
+  ],
+};
