d417905de9162e041792c3a5472140ad8cf8816e
lubien <lubien1996@gmail.com>
2016-09-16 11:09:07 -0300

    Reduce webpack-dev-server output to minimal
    Closes #21

--- server/app.js
+++ server/app.js
@@ -53,6 +53,17 @@ if (config.is.dev) {
     hot: true,
     publicPath: webpackConfig.output.publicPath,
     headers: { 'Access-Control-Allow-Origin': '*' },
+    quiet: false,
+    noInfo: false,
+    stats: {
+      assets: false,
+      colors: true,
+      version: false,
+      hash: false,
+      timings: false,
+      chunks: false,
+      chunkModules: false,
+    },
   });
   webpackServer.listen(8080, config.HOST, () => {
     console.log(`Webpack server listening at ${config.HOST}:8080`);
