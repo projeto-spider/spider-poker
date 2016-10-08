4952aea75c1f5fabbb9132984505f1f953eb0eac
lubien <lubien1996@gmail.com>
2016-10-08 01:06:34 -0300

    Prevent rehydratation fail when there's no server side render

--- client/entry.prod.js
+++ client/entry.prod.js
@@ -5,7 +5,7 @@ import App from './App';
 import store from './store';
 
 // eslint-disable-next-line
-StyleSheet.rehydrate(JSON.parse(window.__RENDERED_CLASS_NAMES__));
+StyleSheet.rehydrate(JSON.parse(window.__RENDERED_CLASS_NAMES__ || {}));
 
 ReactDOM.render(
   <App store={store} />,
