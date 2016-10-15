48f845bc7d89e8a0cca7d718d6ab5926753935f8
lubien <lubien1996@gmail.com>
2016-10-15 18:53:40 -0300

    Stop using transit

--- client/store/dev.js
+++ client/store/dev.js
@@ -1,6 +1,5 @@
 import { createStore, compose } from 'redux';
 import { fromJS } from 'immutable';
-import transit from 'transit-immutable-js';
 import rootReducer from './root-reducer';
 
 const enhancers = compose(
@@ -9,35 +8,8 @@ const enhancers = compose(
 );
 
 const store = createStore(
-    rootReducer, loadState(), enhancers
+    rootReducer, fromJS({}), enhancers
 );
 
-// Thanks to chaintan17
-// See: https://github.com/mxstbr/react-boilerplate/issues/396
-store.subscribe(() => {
-  try {
-    const state = store.getState();
-    const serializedState = transit.toJSON(state);
-    // eslint-disable-next-line
-    localStorage.setItem('state', serializedState);
-  } catch (err) {
-    console.log('Could not save current redux state', err);
-  }
-});
-
 export default store;
-
-function loadState() {
-  try {
-    const serializedState = localStorage.getItem('state');
-
-    if (serializedState === null) {
-      return fromJS({});
-    }
-
-    return transit.fromJSON(serializedState);
-  } catch (err) {
-    return fromJS({});
-  }
-}
 
