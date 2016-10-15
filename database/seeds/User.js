0fbe5d1d86384ed34b14d57c81368c2c35c2cd74
lubien <lubien1996@gmail.com>
2016-10-15 10:12:37 -0300

    Add more fixtures to users for tests

--- database/seeds/User.js
+++ database/seeds/User.js
@@ -4,11 +4,19 @@ const User = use('App/Model/User');
 
 class UserSeeder {
 	* run() {
-		yield User.create({
+		yield User.createMany([{
 			username: 'admin',
 			password: 'admin',
 			email: 'admin@gmail.com',
-		});
+		}, {
+			username: 'lubien',
+			password: 'lubien',
+			email: 'lubien1996@gmail.com',
+		}, {
+			username: 'john-doe',
+			password: 'john-doe',
+			email: 'john.doe@gmail.com',
+		}]);
 	}
 }
 
