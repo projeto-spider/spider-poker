0e2d7fa5681b314c54ade28e78bde5650f2c1678
lubien <lubien1996@gmail.com>
2016-10-15 11:10:46 -0300

    Hide fields from profiles

--- app/Model/Profile.js
+++ app/Model/Profile.js
@@ -3,6 +3,15 @@
 const Lucid = use('Lucid');
 
 class Profile extends Lucid {
+	static get hidden() {
+		return [
+			'id',
+			'user_id',
+			'created_at',
+			'updated_at',
+		];
+	}
+
 	users() {
 		return this.belongsTo('App/Model/User');
 	}
