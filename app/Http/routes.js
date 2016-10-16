a9713611c5aa8777591cb5aafd8083265591cea1
lubien <lubien1996@gmail.com>
2016-10-16 18:25:56 -0300

    Match fallback routes as ReactController

--- app/Http/routes.js
+++ app/Http/routes.js
@@ -17,12 +17,13 @@ Route.group('user', () => {
 		});
 }).prefix('/api');
 
-Route.get('/', 'ReactController.main');
-
 // Widgets
 
 Route.group('widgets', () => {
 	Route
 		.get('/redmine/issues', 'Widgets/RedmineController.issues');
 }).prefix('/widgets');
+
+// Fallback all other GET requests to rendering our React app
+Route.get('*', 'ReactController.main');
 
