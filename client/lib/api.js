a1fa287fbf5bc54f1323deda1349a21617a38585
lubien <lubien1996@gmail.com>
2016-10-17 17:50:36 -0300

    Setup basic API adapter

---
+++ client/lib/api.js
@@ -0,0 +1,45 @@
+import axios from 'axios';
+
+export function isExpired(expires) {
+	return expires <= Math.floor(Date.now() / 1000) + 500;
+}
+
+export function request(url, options) {
+	const req = axios({
+		url,
+		...options
+	});
+
+	if (options.rawResponse) {
+		return req;
+	}
+
+	return req
+		.then(({data}) => data);
+}
+
+['get', 'post', 'put', 'patch', 'head', 'delete']
+	.forEach(method => {
+		request[method] =
+			(url, opts) => request(url, Object.assign({}, opts, {method}));
+	});
+
+const api = {
+	auth: {
+		valid(token) {
+			return request.get('/api/auth/valid', {
+				headers: {authorization: `Bearer ${token}`},
+			});
+		},
+		login(username, password) {
+			return request.post('/api/auth/authorize', {
+				data: {username, password},
+			});
+		},
+		register(data) {
+			return request.post('/api/auth/register', {data});
+		},
+	},
+};
+
+export {api};
