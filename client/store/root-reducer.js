0bc0a17a2ddbf4bd3691d7d47effedf4e4da1d74
lubien <lubien1996@gmail.com>
2016-10-18 11:22:44 -0300

    Add loading bar reducer

--- client/store/root-reducer.js
+++ client/store/root-reducer.js
@@ -1,11 +1,17 @@
-import { combineReducers } from 'redux-immutable';
+import { combineReducers } from 'redux';
 import reduceReducers from 'reduce-reducers';
+import {loadingBarReducer as loadingBar} from 'react-redux-loading-bar';
 
 import config from '../reducers/config';
 import stories from '../reducers/stories';
+import auth from '../reducers/auth';
 
 const rootReducer = reduceReducers(
-  combineReducers({ config, stories }),
+	combineReducers({
+		config,
+		stories,
+		loadingBar,
+	}),
 );
 
 export default rootReducer;
