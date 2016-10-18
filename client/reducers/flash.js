d12ee46540cd94b8e3b9d2744e56c480dfe78e5c
lubien <lubien1996@gmail.com>
2016-10-18 18:27:58 -0300

    Add flash messages reducer

---
+++ client/reducers/flash.js
@@ -0,0 +1,48 @@
+import {LOGIN_REJECTED} from './auth';
+
+const INITIAL_STATE = [];
+
+// Action types
+export const CLEAR = 'app/flash/CLEAR';
+// Message types
+export const FLASH_ALL = 'app/scope/FLASH_ALL';
+export const AUTH_INVALID = 'app/flash/AUTH_INVALID';
+// Flash message levels
+export const FLASH_INFO = 'app/flash/FLASH_INFO';
+export const FLASH_WARNING = 'app/flash/FLASH_WARNING';
+export const FLASH_ERROR = 'app/flash/FLASH_ERROR';
+
+export default function flashReducer(state = INITIAL_STATE, {type, payload = {}}) {
+	switch(type) {
+		case CLEAR: {
+			const type = payload;
+
+			if (type === FLASH_ALL) {
+				return [];
+			}
+
+			return state
+				.filter(message => message.type !== type);
+		}
+		case LOGIN_REJECTED: {
+			return [
+				...state,
+				{
+					type: AUTH_INVALID,
+					level: FLASH_ERROR,
+					text: 'Invalid credentials',
+				}
+			];
+		}
+		default: {
+			return state;
+		}
+	}
+}
+
+export function clear(type = FLASH_ALL) {
+	return {
+		type: CLEAR,
+		payload: type,
+	};
+}
--- client/store/root-reducer.js
+++ client/store/root-reducer.js
@@ -5,12 +5,14 @@ import {loadingBarReducer as loadingBar} from 'react-redux-loading-bar';
 import config from '../reducers/config';
 import stories from '../reducers/stories';
 import auth from '../reducers/auth';
+import flash from '../reducers/flash';
 
 const rootReducer = reduceReducers(
 	combineReducers({
 		config,
 		stories,
 		loadingBar,
+		flash,
 	}),
 );
 
