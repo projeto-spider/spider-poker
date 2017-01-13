ad2b8564a243df559ef8e67283ba3631fa32dfbf
lubien <lubien1996@gmail.com>
2017-01-13 03:19:36 -0300

    Use JaResource at organization controller

--- web/controllers/organization_controller.ex
+++ web/controllers/organization_controller.ex
@@ -1,125 +1,52 @@
 defmodule Poker.OrganizationController do
   use Poker.Web, :controller
+  use JaResource
 
-  alias Poker.{Organization, OrganizationUser}
+  alias Poker.{User, Organization, OrganizationUser}
 
   plug :ensure_authenticated when action in [:create, :update, :delete]
-  plug :preload_session when action in [:create, :show, :update, :delete]
-  plug :prefetch_organization when action in [:show, :update, :delete]
-  plug :can_see_organization when action in [:show]
-  plug :can_modify_organization when action in [:update, :delete]
+  plug :preload_session
+  plug JaResource
 
-  def index(conn, _params) do
-    organizations = Repo.all(Organization)
-    render(conn, "index.json", organizations: organizations)
+  # Scope
+
+  def records(conn) do
+    scope conn, Organization
   end
 
-  def create(conn, %{"organization" => organization_params}) do
-    changeset = Organization.registration_changeset(%Organization{}, organization_params)
+  # Handlers
 
-    case Repo.insert(changeset) do
-      {:ok, organization} ->
-        logged_in = conn.assigns.logged_in
-        role_params = %{"organization_id" => organization.id,
-                        "user_id" => logged_in.id, "role" => "owner"}
-        role = OrganizationUser.registration_changeset(%OrganizationUser{}, role_params)
+  def handle_create(conn, attributes) do
+    changeset = Organization.create_changeset %Organization{}, attributes
 
-        Repo.insert! role
+    with {:ok, organization} <- Repo.insert(changeset) do
+      owner = conn.assigns.current_user
 
-        conn
-        |> put_status(:created)
-        |> put_resp_header("location", organization_path(conn, :show, organization))
-        |> render("show.json", organization: organization)
-      {:error, changeset} ->
-        conn
-        |> put_status(:unprocessable_entity)
-        |> render(Poker.ChangesetView, "error.json", changeset: changeset)
+      role_params = %{"organization_id" => organization.id,
+                      "user_id" => owner.id,
+                      "role" => "owner"}
+
+      role = OrganizationUser.registration_changeset %OrganizationUser{}, role_params
+
+      Repo.insert! role
+
+      organization
     end
   end
 
-  def show(conn, _params) do
-    organization = conn.assigns.organization
-    render(conn, "show.json", organization: organization)
+  def handle_update(conn, organization, attributes) do
+    authorize! conn, organization
+
+    Organization.update_changeset organization, attributes
   end
 
-  def update(conn, %{"organization" => organization_params}) do
-    organization = conn.assigns.organization
-    changeset = Organization.update_changeset(organization, organization_params)
+  def handle_delete(conn, organization) do
+    authorize! conn, organization
 
-    case Repo.update(changeset) do
-      {:ok, organization} ->
-        render(conn, "show.json", organization: organization)
-      {:error, changeset} ->
-        conn
-        |> put_status(:unprocessable_entity)
-        |> render(Poker.ChangesetView, "error.json", changeset: changeset)
-    end
+    super conn, organization
   end
 
-  def delete(conn, _params) do
-    organization = conn.assigns.organization
+  # Filters
 
-    # Here we use delete! (with a bang) because we expect
-    # it to always work (and if it does not, it will raise).
-    Repo.delete!(organization)
-
-    send_resp(conn, :no_content, "")
-  end
-
-  # Helpers
-
-  defp prefetch_organization(%{params: %{"id" => name}} = conn, _opts) do
-    case Repo.get_by(Organization, name: name) do
-      nil ->
-        conn
-        |> send_resp(:not_found, "Organization doesn't exist")
-      organization ->
-        conn
-        |> assign(:organization, organization)
-      end
-  end
-
-  defp can_see_organization(conn, _opts) do
-    organization = conn.assigns.organization
-
-    if not organization.private do
-      conn
-    else
-      case conn.assigns.logged_in do
-        nil ->
-          conn
-          |> send_resp(:unauthorized, "Cannot see this organization")
-        logged_in ->
-          query = from it in OrganizationUser,
-                  where: it.user_id == ^logged_in.id,
-                  where: it.organization_id == ^organization.id
-
-          case Repo.one(query) do
-            nil ->
-              conn
-              |> send_resp(:unauthorized, "Cannot see this organization")
-            _ ->
-              conn
-          end
-      end
-    end
-  end
-
-  defp can_modify_organization(conn, _opts) do
-    logged_in = conn.assigns.logged_in
-    organization = conn.assigns.organization
-
-    query = from it in OrganizationUser,
-            where: it.user_id == ^logged_in.id,
-            where: it.organization_id == ^organization.id,
-            where: it.role == "owner"
-
-    case Repo.one(query) do
-      nil ->
-        conn
-        |> send_resp(:unauthorized, "Cannot modify this organization.")
-      _ ->
-        conn
-    end
-  end
+  filterable_by ["id", "name", "private"]
 end
