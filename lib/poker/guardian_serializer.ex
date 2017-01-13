f175b99110dee709332ab4bffd26879f8d10ca39
lubien <lubien1996@gmail.com>
2017-01-12 23:11:06 -0300

    Update guardian_serializer to preload profile

--- lib/poker/guardian_serializer.ex
+++ lib/poker/guardian_serializer.ex
@@ -7,6 +7,6 @@ defmodule Poker.GuardianSerializer do
   def for_token(user = %User{}), do: {:ok, "User: #{user.id}"}
   def for_token(_), do: {:error, "Unknown resource type"}
 
-  def from_token("User: " <> id), do: {:ok, Repo.get(User.with_profile, id)}
+  def from_token("User: " <> id), do: {:ok, Repo.get(User, id, preload: [:profile])}
   def from_token(_), do: {:error, "Unknown resource type"}
 end
