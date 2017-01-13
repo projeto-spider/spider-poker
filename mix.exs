30ea2e5372f0c47da0e2e7a9e614d214523d8a3c
lubien <lubien1996@gmail.com>
2017-01-13 03:13:14 -0300

    Update Ecto version

--- mix.exs
+++ mix.exs
@@ -33,6 +33,7 @@ defmodule Poker.Mixfile do
     [{:phoenix, "~> 1.2.1"},
      {:phoenix_pubsub, "~> 1.0"},
      {:phoenix_ecto, "~> 3.0"},
+     {:ecto, "~> 2.1.2"},
      {:postgrex, ">= 0.0.0"},
      {:phoenix_html, "~> 2.6"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
