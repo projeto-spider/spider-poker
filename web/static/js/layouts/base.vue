34a41ced984771df70cddf447abee961cd8b8104
lubien <lubien1996@gmail.com>
2017-01-28 19:25:54 -0300

    Rename Dashboard to BaseLayout

---
+++ web/static/js/layouts/base.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <navbar/>
+    <router-view/>
+    <footer-component/>
+  </div>
+</template>
+
+<script>
+  import {
+    Navbar,
+    Footer
+  } from 'app/components'
+
+  export default {
+    name: 'BaseLayout',
+
+    components: {Navbar, 'footer-component': Footer}
+  }
+</script>
