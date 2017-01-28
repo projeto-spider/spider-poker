94fc67fa8fd392266ad5f9bbfc48181cc135da8a
lubien <lubien1996@gmail.com>
2017-01-28 19:22:24 -0300

    Rename RouterBase to App

---
+++ web/static/js/components/app.vue
@@ -0,0 +1,17 @@
+<template>
+  <router-view></router-view>
+</template>
+
+<script>
+  export default {
+    name: 'App',
+  }
+</script>
+
+<style lang="sass">
+@import '~font-awesome/css/font-awesome.css'
+@import '~bulma'
+
+main > .container
+  padding: 10px 0
+</style>
