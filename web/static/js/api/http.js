88996f526f3a4029af92379ce2bcfd6eebe23f52
lubien <lubien1996@gmail.com>
2017-01-31 00:55:16 -0300

    Http request helper

---
+++ web/static/js/api/http.js
@@ -0,0 +1,19 @@
+import Vue from 'vue';
+import VueResource from 'vue-resource';
+import store from 'app/store';
+import parse from './parse'
+
+Vue.use(VueResource)
+
+Vue.http.options.root = '/api'
+Vue.http.headers.common['accept'] = 'application/vnd.api+json'
+Vue.http.headers.common['content-type'] = 'application/vnd.api+json'
+
+Vue.http.interceptors.push((request, next) => {
+  request.headers.set('Authorization', store.state.auth.token)
+  next()
+})
+
+const {resource, http} = Vue
+
+export {resource, http}
