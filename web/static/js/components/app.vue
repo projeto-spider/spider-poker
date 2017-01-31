340a9575258644e0811d2bdf3b7b87ea71d9f801
lubien <lubien1996@gmail.com>
2017-01-31 00:59:17 -0300

    Add nprogress at app.vue

--- web/static/js/components/app.vue
+++ web/static/js/components/app.vue
@@ -11,7 +11,14 @@
 <style lang="sass">
 @import '~font-awesome/css/font-awesome.css'
 @import '~bulma'
+@import '~nprogress/nprogress.css'
 
 main > .container
   padding: 10px 0
+
+#nprogress .bar
+  background-color: #3273DC
+
+#nprogress .spinner
+  display: none
 </style>
