55db6f0ca44c226676d4ac1478df097adbd4d628
lubien <lubien1996@gmail.com>
2017-01-31 00:58:03 -0300

    Remove VueResource from app.js

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -3,13 +3,11 @@ import 'babel-polyfill';
 import store from 'app/store';
 import VueRouter from 'vue-router';
 import routes from './config/routes';
-import VueResource from 'vue-resource';
 import VueValidator from 'vue-validator';
 import NProgress from 'vue-nprogress';
 import {App} from 'app/components'
 
 Vue.use(VueRouter);
-Vue.use(VueResource);
 Vue.use(VueValidator);
 Vue.use(NProgress);
 
@@ -17,13 +15,6 @@ const environment = process.env.NODE_ENV;
 
 Vue.config.debug = (environment === 'development');
 Vue.config.devtools = (environment === 'development');
-
-Vue.http.interceptors.push((request, next) => {
-  request.headers.set('Authorization', store.state.auth.token);
-  request.headers.set('Content-Type', 'application/vnd.api+json');
-  request.headers.set('Accepts', 'application/vnd.api+json');
-  next();
-})
 
 const router = new VueRouter({
   mode: 'history',
@@ -37,7 +28,9 @@ const nprogress = new NProgress();
 const VueApp = new Vue({
   el: "#content",
   render: r => r(App),
-  router, store, nprogress
+  router,
+  store,
+  nprogress
 })
 
 if (window && environment === 'development') {
