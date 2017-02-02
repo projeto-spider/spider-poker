c86cba992e7a5c0d401c97aa7a7358ee7bdc9043
leochrisis <leonardo.chfc@gmail.com>
2017-02-02 15:26:24 -0300

    Create API wrapper for organizations

--- web/static/js/api/organizations.js
+++ web/static/js/api/organizations.js
@@ -1,4 +1,8 @@
 import Vue from 'vue';
+import {http, resource} from './http'
+import parse from './parse'
+
+const organizations = resource('organizations{/id}')
 
 export default {
   all() {
@@ -6,9 +10,26 @@ export default {
       .then(r => r.json());
   },
 
-  show(id) {
-    return Vue.http.get(`/api/organizations/${id}`)
-      .then(r => r.json());
+create(organization) {
+    return organizations.save({}, {data: {attributes: organization}})
+      .then(r => r.json())
+      .then(parse)
+  },
+
+show(name) {
+    return organizations.query({'filter[name]': name})
+      .then(r => r.json())
+      .then(parse)
+  },
+
+  update(id, attributes) {
+    return http.put(`organizations/${id}`, {data: {attributes}})
+      .then(r => r.json())
+      .then(parse)
+  },
+
+  delete(id) {
+    return organizations.delete({id})
   },
 
   projects: {
