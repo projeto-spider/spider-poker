4db1ff3b9eef148417f25654acb43d2d5b77130a
lubien <lubien1996@gmail.com>
2017-03-30 09:06:20 -0300

    Small refactor at guardian_serializer

--- lib/poker/web/helpers/guardian_serializer.ex
+++ lib/poker/web/helpers/guardian_serializer.ex
@@ -1,12 +1,15 @@
 defmodule Poker.Web.Helpers.GuardianSerializer do
   @behaviour Guardian.Serializer
 
-  alias Poker.Repo
-  alias Poker.User
+  alias Poker.{Repo, User}
 
-  def for_token(user = %User{}), do: {:ok, "User: #{user.id}"}
-  def for_token(_), do: {:error, "Unknown resource type"}
+  def for_token(user = %User{}), do:
+    {:ok, "User: #{user.id}"}
+  def for_token(_), do:
+    {:error, "Unknown resource type"}
 
-  def from_token("User: " <> id), do: {:ok, Repo.get(User, id, preload: [:profile])}
-  def from_token(_), do: {:error, "Unknown resource type"}
+  def from_token("User: " <> id), do:
+    {:ok, Repo.get(User, id)}
+  def from_token(_), do:
+    {:error, "Unknown resource type"}
 end
