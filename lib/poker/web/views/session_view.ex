9e4cb9269c6a9028e538ed39ccaaf8c2da00a5be
lubien <lubien1996@gmail.com>
2017-03-30 09:28:05 -0300

    Remove unnused code from SessionView

--- lib/poker/web/views/session_view.ex
+++ lib/poker/web/views/session_view.ex
@@ -1,15 +1,7 @@
 defmodule Poker.Web.SessionView do
   use Poker.Web, :view
 
-  def render("token.json", %{token: token}) do
-    %{data: %{token: token}}
-  end
-
-  def render("show.json", %{session: session}) do
-    %{data: render_one(session, Poker.Web.SessionView, "session.json")}
-  end
-
-  def render("session.json", %{session: session}) do
-    %{id: session.id}
+  def render("token.json", %{token: token, expiry: expiry}) do
+    %{data: %{token: token, expiry: expiry}}
   end
 end
