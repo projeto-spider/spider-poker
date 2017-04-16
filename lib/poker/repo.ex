10277b8f89efeb34698cf235ea79891ae573648f
lubien <lubien1996@gmail.com>
2017-04-16 14:51:17 -0300

    Add Repo soft getter
    To match fallback_controller behavior

--- lib/poker/repo.ex
+++ lib/poker/repo.ex
@@ -1,4 +1,22 @@
 defmodule Poker.Repo do
   use Ecto.Repo, otp_app: :poker
   use Scrivener, page_size: 50
+
+  def soft_get(queryable, id, opts \\ []) do
+    case get(queryable, id, opts) do
+      nil ->
+        {:error, :not_found}
+      schema ->
+        {:ok, schema}
+    end
+  end
+
+  def soft_get_by(queryable, clauses, opts \\ []) do
+    case get_by(queryable, clauses, opts) do
+      nil ->
+        {:error, :not_found}
+      schema ->
+        {:ok, schema}
+    end
+  end
 end
