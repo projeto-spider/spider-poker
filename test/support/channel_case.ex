0c47e4738774af6b1a8567bd11dee77bef420bff
lubien <lubien1996@gmail.com>
2017-04-16 18:01:05 -0300

    Update test support

--- test/support/channel_case.ex
+++ test/support/channel_case.ex
@@ -1,11 +1,11 @@
-defmodule Poker.ChannelCase do
+defmodule Poker.Web.ChannelCase do
   @moduledoc """
   This module defines the test case to be used by
   channel tests.
 
   Such tests rely on `Phoenix.ChannelTest` and also
   import other functionality to make it easier
-  to build and query models.
+  to build common datastructures and query the data layer.
 
   Finally, if the test case interacts with the database,
   it cannot be async. For this reason, every test runs
@@ -20,24 +20,18 @@ defmodule Poker.ChannelCase do
       # Import conveniences for testing with channels
       use Phoenix.ChannelTest
 
-      alias Poker.Repo
-      import Ecto
-      import Ecto.Changeset
-      import Ecto.Query
-
-
       # The default endpoint for testing
-      @endpoint Poker.Endpoint
+      @endpoint Poker.Web.Endpoint
     end
   end
+
 
   setup tags do
     :ok = Ecto.Adapters.SQL.Sandbox.checkout(Poker.Repo)
-
     unless tags[:async] do
       Ecto.Adapters.SQL.Sandbox.mode(Poker.Repo, {:shared, self()})
     end
-
     :ok
   end
+
 end
--- test/support/conn_case.ex
+++ test/support/conn_case.ex
@@ -1,11 +1,11 @@
-defmodule Poker.ConnCase do
+defmodule Poker.Web.ConnCase do
   @moduledoc """
   This module defines the test case to be used by
   tests that require setting up a connection.
 
   Such tests rely on `Phoenix.ConnTest` and also
   import other functionality to make it easier
-  to build and query models.
+  to build common datastructures and query the data layer.
 
   Finally, if the test case interacts with the database,
   it cannot be async. For this reason, every test runs
@@ -19,26 +19,20 @@ defmodule Poker.ConnCase do
     quote do
       # Import conveniences for testing with connections
       use Phoenix.ConnTest
-
-      alias Poker.Repo
-      import Ecto
-      import Ecto.Changeset
-      import Ecto.Query
-
-      import Poker.Router.Helpers
+      import Poker.Web.Router.Helpers
 
       # The default endpoint for testing
-      @endpoint Poker.Endpoint
+      @endpoint Poker.Web.Endpoint
     end
   end
+
 
   setup tags do
     :ok = Ecto.Adapters.SQL.Sandbox.checkout(Poker.Repo)
-
     unless tags[:async] do
       Ecto.Adapters.SQL.Sandbox.mode(Poker.Repo, {:shared, self()})
     end
-
     {:ok, conn: Phoenix.ConnTest.build_conn()}
   end
+
 end
