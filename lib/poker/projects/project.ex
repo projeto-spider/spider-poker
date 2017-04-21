58914505f54cfcfb37012039d18d24a1ae40b1b0
lubien <lubien1996@gmail.com>
2017-04-21 00:03:33 -0300

    Setup basic Projects context

---
+++ lib/poker/projects/project.ex
@@ -0,0 +1,49 @@
+defmodule Poker.Projects.Project do
+  @moduledoc false
+  use Poker.Web, :model
+  use Ecto.Schema
+
+  alias Poker.Pattern
+  alias Poker.Projects.Project
+
+  schema "projects" do
+    field :name, :string
+    field :display_name, :string
+    field :description, :string
+    field :private, :boolean, default: false
+    belongs_to :organization, Poker.Organizations.Organization
+    has_many :projects_members, Poker.Projects.Member, on_delete: :delete_all
+    has_many :users, through: [:projects_members, :users]
+
+    timestamps()
+  end
+
+  def changeset(%Project{} = project, attrs \\ %{}) do
+    project
+    |> cast(attrs, [:name, :display_name, :description, :private])
+  end
+
+  def validate(changeset) do
+    changeset
+    |> validate_required([:name, :display_name])
+    |> validate_length(:name, min: 6, max: 64)
+    |> validate_length(:display_name, min: 1)
+    |> validate_format(:name, Pattern.username())
+    |> unique_constraint(:name)
+    |> assoc_constraint(:organization)
+  end
+
+  def default_display_name(changeset) do
+    name         = get_change(changeset, :name)
+    display_name = get_change(changeset, :display_name)
+
+    case {name, display_name} do
+      {name, nil} when is_binary(name) ->
+        changeset
+        |> put_change(:display_name, name)
+
+      _ ->
+        changeset
+    end
+  end
+end
---
+++ lib/poker/projects/projects.ex
@@ -0,0 +1,123 @@
+defmodule Poker.Projects do
+  @moduledoc false
+  import Ecto.{Query, Changeset}, warn: false
+  alias Poker.Repo
+  alias Poker.Pattern
+  alias Poker.Accounts.User
+  alias Poker.Projects.Project
+  alias Poker.Projects.Member
+
+  def query, do: Project
+
+  def query_public do
+    Project
+    |> where(private: false)
+  end
+
+  def query_visible_to(%User{id: user_id}) do
+    from p in Project,
+    left_join: m in assoc(p, :projects_members),
+    where: p.private == false or
+           m.user_id == ^user_id
+  end
+  def query_visible_to(_), do: query_public()
+
+  def get(scope \\ Project, id) do
+    if Pattern.numeric?(id) do
+      Repo.soft_get(scope, id)
+    else
+      Repo.soft_get_by(scope, name: id)
+    end
+  end
+
+  def create(attrs \\ %{}) do
+    %Project{}
+    |> Project.changeset(attrs)
+    |> Project.default_display_name
+    |> Project.validate
+    |> Repo.insert
+  end
+
+  def update(%Project{} = project, attrs) do
+    project
+    |> Project.changeset(attrs)
+    |> Project.validate
+    |> Repo.update
+  end
+
+  def delete(%Project{} = project) do
+    Repo.delete(project)
+  end
+
+  def can_see?(proj_id, user_id) do
+    scope =
+      if is_nil(user_id) do
+        query_public()
+      else
+        query_visible_to(%User{id: user_id})
+      end
+
+    query =
+      from(m in scope, select: count(m.id, :distinct))
+
+    case Repo.soft_get(query, proj_id) do
+      {:ok, 1} ->
+        :ok
+      {:ok, _} ->
+        {:error, :not_found}
+    end
+  end
+
+  # Membership
+
+  def query_members(proj_id) do
+    Member
+    |> preload([:user])
+    |> where(project_id: ^proj_id)
+  end
+
+  def get_member(proj_id, id) do
+    query =
+      Member
+      |> preload([:user])
+
+    if Pattern.numeric?(id) do
+      Repo.soft_get_by(query, project_id: proj_id, user_id: id)
+    else
+      Repo.soft_get_by(query, project_id: proj_id, username: id)
+    end
+  end
+
+  def add_member(proj_id, user_id, role \\ "team") do
+    attrs = %{project_id: proj_id, user_id: user_id, role: role}
+
+    %Member{}
+    |> Member.changeset(attrs)
+    |> Member.validate
+    |> Member.validate_duplicated
+    |> Repo.insert
+  end
+
+  def update_member(%Member{} = member, attrs) do
+    member
+    |> Member.changeset(attrs)
+    |> Member.validate
+    |> Repo.update
+  end
+
+  def delete_member(proj_id, user_id) do
+    with {:ok, member} <- get_member(proj_id, user_id) do
+      Repo.delete(member)
+    end
+  end
+
+  def member?(proj_id, user_id) do
+    query =
+      from m in Member,
+      where: m.project_id == ^proj_id and
+             m.user_id == ^user_id,
+      select: count(m.id)
+
+    Repo.one!(query) > 0
+  end
+end
