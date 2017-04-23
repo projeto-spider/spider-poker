8a49e992b5a5c2a1fc295537cbdfe6d224dda088
lubien <lubien1996@gmail.com>
2017-04-23 17:29:36 -0300

    Disable webpack HMR

--- assets/build/webpack.dev.conf.js
+++ assets/build/webpack.dev.conf.js
@@ -22,7 +22,7 @@ module.exports = merge(baseWebpackConfig, {
       'process.env': config.dev.env
     }),
     // https://github.com/glenjamin/webpack-hot-middleware#installation--usage
-    new webpack.HotModuleReplacementPlugin(),
+    // new webpack.HotModuleReplacementPlugin(),
     new webpack.NoEmitOnErrorsPlugin(),
     // https://github.com/ampedandwired/html-webpack-plugin
     new HtmlWebpackPlugin({
