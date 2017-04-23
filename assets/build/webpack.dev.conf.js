e0e280c3c7a249d8383c3c271241e1df97a656aa
lubien <lubien1996@gmail.com>
2017-04-23 17:39:11 -0300

    Disable hot-reload entry chunks insertion

--- assets/build/webpack.dev.conf.js
+++ assets/build/webpack.dev.conf.js
@@ -7,9 +7,9 @@ var HtmlWebpackPlugin = require('html-webpack-plugin')
 var FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin')
 
 // add hot-reload related code to entry chunks
-Object.keys(baseWebpackConfig.entry).forEach(function (name) {
-  baseWebpackConfig.entry[name] = ['./build/dev-client'].concat(baseWebpackConfig.entry[name])
-})
+// Object.keys(baseWebpackConfig.entry).forEach(function (name) {
+//   baseWebpackConfig.entry[name] = ['./build/dev-client'].concat(baseWebpackConfig.entry[name])
+// })
 
 module.exports = merge(baseWebpackConfig, {
   module: {
