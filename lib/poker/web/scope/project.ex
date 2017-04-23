20cdc2218f10ebd63edad54ffe5bab451a2de888
lubien <lubien1996@gmail.com>
2017-04-23 01:59:25 -0300

    Update Projects scope logic

--- lib/poker/web/scope/project.ex
+++ lib/poker/web/scope/project.ex
@@ -1,36 +1,41 @@
 defmodule Poker.Web.Scope.Project do
+  @moduledoc false
   use Poker.Web, :scope
 
-  alias Poker.{User, Project}
+  alias Poker.Pattern
+  alias Poker.Projects
 
   @filters   ["name", "private", "organization_id"]
   @searchers ["name", "display_name", "description"]
 
-  def default(conn) do
-    base =
-      Project
-      |> preload([:organization])
-      |> filter_params(conn, @filters)
-      |> search_params(conn, @searchers)
-      |> distinct(true)
+  def filters(query, conn) do
+    query
+    |> preload([:organization])
+    |> filter_params(conn, @filters)
+    |> search_params(conn, @searchers)
+  end
 
+  def nested(query, conn) do
     case nested_resource(conn) do
       {"organizations", org_id} ->
-        base
-        |> where(organization_id: ^org_id)
+        if Pattern.numeric?(org_id) do
+          query
+          |> where(organization_id: ^org_id)
+        else
+          from p in query,
+          join: o in assoc(p, :organization),
+          where: o.name == ^org_id
+        end
 
       _ ->
-        base
+        query
     end
   end
 
-  def scope(conn, %User{id: user_id}, _action, _data) do
-    default(conn)
-    |> Project.where_user_can_see(user_id)
-  end
-
-  def scope(conn, nil, _action, _data) do
-    default(conn)
-    |> Project.where_is_public
+  def scope(conn, user, _action, _data) do
+    user
+    |> Projects.query_visible_to
+    |> nested(conn)
+    |> filters(conn)
   end
 end
