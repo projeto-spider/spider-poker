7bae6be0e7030cbd85afc012a5db3444fb331eb3
lubien <lubien1996@gmail.com>
2017-05-13 00:15:01 -0300

    Create Dropdown component

---
+++ assets/src/components/dropdown.vue
@@ -0,0 +1,19 @@
+<template>
+  <my-vue-dropdown
+    :visible="open"
+    @clickOut="handleClickOut"
+    :position="position"
+    :animation="animation"
+  >
+    <div @click="handleClick">
+      <slot></slot>
+    </div>
+
+    <div slot="dropdown" class="dropdown">
+      <slot name="dropdown-menu"></slot>
+    </div>
+  </my-vue-dropdown>
+</template>
+
+<script src="./dropdown.js"></script>
+<style lang="sass" src="./dropdown.sass"></style>
