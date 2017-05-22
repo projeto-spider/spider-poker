de1202df20ddd5c1b69ab1f8252a0309f5d0928e
lubien <lubien1996@gmail.com>
2017-05-22 17:30:40 -0300

    Remove unnused clickaway mixin

--- assets/src/components/app-aside.js
+++ assets/src/components/app-aside.js
@@ -1,12 +1,9 @@
 import {mapGetters, mapActions} from 'vuex'
-import {mixin as clickaway} from 'vue-clickaway'
 import {Gravatar, Dropdown, LoggedUserDropdown} from 'app/components'
 import Sidebar from './sidebar.vue'
 
 export default {
   name: 'AppSidebar',
-
-  mixins: [clickaway],
 
   components: {Sidebar, Gravatar, Dropdown, LoggedUserDropdown},
 
