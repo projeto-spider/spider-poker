60c3e4fda854ed2f9cf7bef8517aea835ac1236c
lubien <lubien1996@gmail.com>
2017-05-26 18:13:49 -0300

    Fix Home and UserEdit padding and margins

--- assets/src/pages/pages/home.vue
+++ assets/src/pages/pages/home.vue
@@ -1,9 +1,9 @@
 <template>
-  <main class="columns">
-    <div class="column">
+  <main class="columns is-marginless">
+    <div class="column is-paddingless">
       <user-block :user="loggedUser"></user-block>
     </div>
-    <div class="column">
+    <div class="column is-paddingless">
       <memberships-block :user="loggedUser"></memberships-block>
     </div>
   </main>
