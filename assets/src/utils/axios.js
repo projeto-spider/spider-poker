669290a5cca9c18acc1c0d367fd6afc4d6395137
lubien <lubien1996@gmail.com>
2017-06-13 04:50:51 -0300

    Setup axios instance

---
+++ assets/src/utils/axios.js
@@ -0,0 +1,36 @@
+import axios from 'axios'
+import store from 'store'
+
+const instance = axios.create({
+  baseURL: '/api',
+
+  validateStatus(status) {
+    return status >= 200 && status < 300
+  }
+})
+
+instance.interceptors.request.use(putAuthToken)
+instance.interceptors.response.use(mergeRedundantData)
+
+export default instance
+
+/*
+*  Inject from store the user token
+*/
+function putAuthToken(request) {
+  request.headers.common['Authorization'] = store.state.auth.token
+  return request
+}
+
+/*
+*  Axios use the key "data" to put the parsed JSON.
+*  Our API also uses "data" to store the main content.
+*  Our API also has the key "meta" for things like pagination.
+*/
+function mergeRedundantData(response) {
+  if (response.data && response.data.data) {
+    return Object.assign(response, response.data)
+  }
+
+  return response
+}
