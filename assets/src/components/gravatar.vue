f51d3f3688c723ed925f5497324e5284ae24f02e
lubien <lubien1996@gmail.com>
2017-06-13 04:55:01 -0300

    Create Gravatar component

---
+++ assets/src/components/gravatar.vue
@@ -0,0 +1,45 @@
+<template>
+  <img :src="url" :class="imageClass"/>
+</template>
+
+<script>
+import gravatar from 'gravatar'
+
+export default {
+  name: 'Gravatar',
+
+  props: {
+    email: {
+      type: String,
+      required: true
+    },
+
+    size: {
+      type: Number,
+      default: 64
+    },
+
+    circle: {
+      type: Boolean,
+      default: false
+    }
+  },
+
+  computed: {
+    url() {
+      return gravatar.url(this.email, {size: this.size})
+    },
+
+    imageClass() {
+      return {
+        'is-round': this.circle
+      }
+    }
+  }
+}
+</script>
+
+<style lang="sass" scoped=true>
+.is-round
+  border-radius: 50%
+</style>
