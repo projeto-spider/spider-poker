78036d7fe6abbb92c68a76cb8957baa5a91cc1d2
lubien <lubien1996@gmail.com>
2017-06-13 08:37:46 -0300

    Use layout with Dashboard modals

--- assets/src/pages/dashboard/modal/add-organization.vue
+++ assets/src/pages/dashboard/modal/add-organization.vue
@@ -1,18 +1,28 @@
 <template>
-  <div>
-    <h4>Creating Organization</h4>
+  <q-layout>
+    <div slot="header" class="toolbar">
+      <q-toolbar-title :padding="1">
+        <button @click="modal.close()">
+          <i>keyboard_arrow_left</i>
+        </button>
 
-    <form @submit.prevent="create">
-      <p>
-        <div class="floating-label">
-          <input required autofocus class="full-width" v-model="name">
-          <label>Name</label>
-        </div>
-      </p>
-    </form>
+        Creating Organization
+      </q-toolbar-title>
+    </div>
 
-    <p><button class="primary" @click="modal.close()">Close</button></p>
-  </div>
+    <div class="layout-view">
+      <div class="layout-padding">
+        <form @submit.prevent="create">
+          <p>
+            <div class="floating-label">
+              <input required autofocus class="full-width" v-model="name">
+              <label>Name</label>
+            </div>
+          </p>
+        </form>
+      </div>
+    </div>
+  </q-layout>
 </template>
 
 <script src="./add-organization.js"></script>
--- assets/src/pages/dashboard/modal/add-project.vue
+++ assets/src/pages/dashboard/modal/add-project.vue
@@ -1,26 +1,37 @@
 <template>
-  <div>
-    <h4>Creating Project</h4>
+  <q-layout>
+    <div slot="header" class="toolbar">
+      <q-toolbar-title :padding="1">
+        <button @click="modal.close()">
+          <i>keyboard_arrow_left</i>
+        </button>
 
-    <form @submit.prevent="create">
-      <p>
-        <q-select
-          type="list"
-          v-model="organization"
-          :options="selectOptions"
-        ></q-select>
-      </p>
+        Creating Project
+      </q-toolbar-title>
+    </div>
 
-      <p>
-        <div class="floating-label">
-          <input required autofocus class="full-width" v-model="name">
-          <label>Name</label>
-        </div>
-      </p>
-    </form>
+    <div class="layout-view">
+      <div class="layout-padding">
+        <form @submit.prevent="create">
+          <p>
+            <q-select
+              type="list"
+              class="full-width"
+              v-model="organization"
+              :options="selectOptions"
+            ></q-select>
+          </p>
 
-    <p><button class="primary" @click="modal.close()">Close</button></p>
-  </div>
+          <p>
+            <div class="floating-label">
+              <input required autofocus class="full-width" v-model="name">
+              <label>Name</label>
+            </div>
+          </p>
+        </form>
+      </div>
+    </div>
+  </q-layout>
 </template>
 
 <script src="./add-project.js"></script>
