342bdbcd9d7d1a9aeb17ab1e95844b1a8f5417b9
lubien <lubien1996@gmail.com>
2017-06-15 16:14:48 -0300

    Setup basic backlog listing

--- assets/src/pages/dashboard/project.js
+++ assets/src/pages/dashboard/project.js
@@ -1,9 +1,50 @@
+import {Toast, Loading} from 'quasar'
+import axios from 'utils/axios'
+
 export default {
   name: 'Project',
+
+  data: () => ({
+    /* Backlog */
+    order: [],
+    stories: {},
+
+    /* Adding Story */
+    title: '',
+    description: ''
+  }),
 
   computed: {
     projectId() {
       return this.$route.params.projectId
+    },
+
+    backlog() {
+      return this.order
+        .map(id => this.stories[id])
+    }
+  },
+
+  created() {
+    Loading.show({
+      message: 'Loading backlog'
+    })
+
+    axios.get(`/projects/${this.projectId}/backlog`)
+      .then(this.handleLoadedBacklog)
+      .catch(this.handleLoadBacklogFail)
+  },
+
+  methods: {
+    handleLoadedBacklog(response) {
+      Loading.hide()
+      this.order = response.meta.order
+      this.stories = response.data
+    },
+
+    handleLoadBacklogFail(error) {
+      Loading.hide()
+      Toast.create.negative('Failed to load backlog')
     }
   }
 }
--- assets/src/pages/dashboard/project.vue
+++ assets/src/pages/dashboard/project.vue
@@ -1,7 +1,55 @@
 <template>
-  <div class="layout-padding items-start">
-    {{projectId}}
+  <div class="layout-padding align-center">
+    <div class="story-form">
+      <div class="list">
+        <div class="item two-lines">
+          <div class="item-content">
+            <div class="stacked-label">
+              <input v-model="title" required class="full-width">
+              <label>New Story Title</label>
+            </div>
+          </div>
+        </div>
+
+        <hr/>
+
+        <div class="item two-lines">
+          <div class="item-content">
+            <div class="stacked-label">
+              <input v-model="description" class="full-width">
+              <label>Description (optional)</label>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div v-for="story in backlog" class="card card-story bg-lime-2">
+      <div class="card-title">
+        {{story.title}}
+
+        <button ref="target" class="clear pull-right">
+          <i>more_vert</i>
+
+          <q-popover ref="popover">
+            <div class="list item-delimiter highlight">
+              <div
+                class="item item-link"
+                @click="doSomething(), $refs.popover.close()"
+              >
+                ...
+              </div>
+            </div>
+          </q-popover>
+        </button>
+      </div>
+
+      <div v-if="story.description" class="card-content">
+        {{story.description}}
+      </div>
+    </div>
   </div>
 </template>
 
 <script src="./project.js"></script>
+<style lang="sass" src="./project.sass"></style>
