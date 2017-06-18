a1e645fc2fffb0261d420efcd50edbe2609f1219
lubien <lubien1996@gmail.com>
2017-06-17 22:18:36 -0300

    Add Backlog.move helper function

--- lib/poker/projects/backlog.ex
+++ lib/poker/projects/backlog.ex
@@ -8,4 +8,14 @@ defmodule Poker.Projects.Backlog do
   def push(backlog, id) do
     Enum.concat(backlog, [id])
   end
+
+  def move(backlog, id, 0),    do: [id | Enum.reject(backlog, &(&1 == id))]
+  def move([], id, _position), do: [id]
+  def move([head|tail], id, position) do
+    if head == id do
+      move(tail, id, position)
+    else
+      [head | move(tail, id, position - 1)]
+    end
+  end
 end
