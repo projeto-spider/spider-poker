08d2447a7311b4d292264e6e23384af37a71eb1b
lubien <lubien1996@gmail.com>
2017-08-28 22:52:49 -0300

    Finish game at ProjectChannel

--- lib/app/web/channels/project_channel.ex
+++ lib/app/web/channels/project_channel.ex
@@ -6,6 +6,10 @@ defmodule App.Web.ProjectChannel do
   alias App.Presence
   alias App.Projects
   alias App.Projects.Project
+  alias App.Projects.Story
+  alias App.Projects.Game
+
+  @state Game.state()
 
   def join("project:" <> project_id, _params, socket) do
     if Projects.member?(project_id, user(socket).id) do
@@ -21,6 +25,11 @@ defmodule App.Web.ProjectChannel do
     {:ok, _} = Presence.track(socket, user(socket).id, %{
       online_at: inspect(System.system_time(:seconds))
     })
+    {:noreply, socket}
+  end
+  def handle_info(:stop_voting, socket) do
+    # TODO: stop voting even if the manager finished the game
+    handle_in("game:voting:stop", 0, socket)
     {:noreply, socket}
   end
 
@@ -57,12 +66,37 @@ defmodule App.Web.ProjectChannel do
     
     {:noreply, socket}
   end
+  def handle_in("substory:delete", %{"parent_id" => parent_id, "story_id" => id}, socket) do
+    parent = Projects.get_story!(parent_id)
+    story = Projects.get_story!(id)
+
+    case Projects.delete_story(parent, story) do
+      {:ok, parent, story} ->
+        broadcast!(socket, "substory:deleted", %{parent_id: parent_id, order: backlog(parent), story_id: story.id})
+      {:error, _reason} ->
+        push(socket, "error", %{reason: "Failed to delete story"})
+    end
+    
+    {:noreply, socket}
+  end
   def handle_in("story:move", %{"story_id" => id, "position" => position}, socket) do
     story = Projects.get_story!(id)
     project = Projects.get_project!(story.project_id)
     case Projects.move_story(project, story.id, position) do
       {:ok, project} ->
         broadcast!(socket, "story:moved", %{order: backlog(project)})
+      {:error, _reason} ->
+        push(socket, "error", %{reason: "Failed to move story"})
+    end
+    
+    {:noreply, socket}
+  end
+  def handle_in("substory:move", %{"parent_id" => parent_id, "story_id" => id, "position" => position}, socket) do
+    parent = Projects.get_story!(parent_id)
+    story = Projects.get_story!(id)
+    case Projects.move_story(parent, story.id, position) do
+      {:ok, parent} ->
+        broadcast!(socket, "substory:moved", %{parent_id: parent_id, order: backlog(parent)})
       {:error, _reason} ->
         push(socket, "error", %{reason: "Failed to move story"})
     end
@@ -100,6 +134,121 @@ defmodule App.Web.ProjectChannel do
 
     {:noreply, socket}
   end
+  def handle_in("game:message", %{"body" => body}, socket) do
+    message = %{user_id: user(socket).id, body: body}
+
+    broadcast! socket, "game:message", %{message: message}
+    {:noreply, socket}
+  end
+  def handle_in("game:voting:start", _, socket) do
+    project = Projects.get_project!(project_id(socket))
+
+    with :ok <- being_manager(project, socket) do
+      game = Projects.get_game!(project.current_game)
+
+      duration = project.votation_time * 60
+      voting_start = System.system_time(:seconds)
+      voting_end = voting_start + duration
+
+      :timer.send_after(duration * 1000, self(), :stop_voting)
+
+      attrs = %{
+        state: @state.voting,
+        votes: %{},
+        voting_start: DateTime.from_unix!(voting_start),
+        voting_end: DateTime.from_unix!(voting_end)
+      }
+      {:ok, game} = Projects.update_game(game, attrs)
+
+      broadcast!(socket, "game:state", %{game: game})
+
+      {:noreply, socket}
+    end
+  end
+  def handle_in("game:voting:stop", _, socket) do
+    project = Projects.get_project!(project_id(socket))
+
+    with :ok <- being_manager(project, socket) do
+      game = Projects.get_game!(project.current_game)
+
+      duration = project.votation_time * 60
+      voting_end = System.system_time(:seconds)
+
+      attrs = %{
+        state: @state.discussion,
+        voting_end: DateTime.from_unix!(voting_end)
+      }
+      {:ok, game} = Projects.update_game(game, attrs)
+
+      broadcast!(socket, "game:state", %{game: game})
+
+      {:noreply, socket}
+    end
+  end
+  def handle_in("game:voting:vote", %{"estimation" => estimation}, socket) do
+    project = Projects.get_project!(project_id(socket))
+    game = Projects.get_game!(project.current_game)
+
+    votes = Map.put(game.votes, user(socket).id, estimation)
+
+    attrs = %{
+      votes: votes
+    }
+    {:ok, game} = Projects.update_game(game, attrs)
+
+    broadcast!(socket, "game:state", %{game: game})
+
+    {:noreply, socket}
+  end
+  def handle_in("game:discussion:score", %{"estimation" => estimation}, socket) do
+    project = Projects.get_project!(project_id(socket))
+
+    with :ok <- being_manager(project, socket) do
+      game = Projects.get_game!(project.current_game)
+      story = Projects.get_story!(game.story_id)
+
+      attrs = %{
+        estimation: estimation
+      }
+      case Projects.update_story(story, attrs) do
+        {:ok, story} ->
+          broadcast!(socket, "story:updated", %{story: story})
+        {:error, _reason} ->
+          push(socket, "error", %{reason: "Failed to update story"})
+      end
+
+      attrs = %{
+        state: @state.idle
+      }
+      {:ok, game} = Projects.update_game(game, attrs)
+
+      broadcast!(socket, "game:state", %{game: game})
+
+      {:noreply, socket}
+    end
+  end
+  def handle_in("game:discussion:create_substory", attrs, socket) do
+    project = Projects.get_project!(project_id(socket))
+
+    with :ok <- being_manager(project, socket) do
+      game = Projects.get_game!(project.current_game)
+      parent = Projects.get_story!(game.story_id)
+
+      case Projects.unshift_story(parent, attrs) do
+        {:ok, parent, story} ->
+          broadcast!(socket, "substory:created", %{
+            parent_id: parent.id,
+            order: backlog(parent),
+            story: story
+          })
+
+        {:error, _reason} ->
+          push(socket, "error", %{reason: "Failed to create story"})
+      end
+
+      {:noreply, socket}
+    end
+  end
 
   defp user(socket), do: socket.assigns.user
 
@@ -108,6 +257,7 @@ defmodule App.Web.ProjectChannel do
   defp project!(socket), do: Projects.get_project!(project_id(socket))
 
   defp backlog(%Project{backlog: backlog}), do: backlog
+  defp backlog(%Story{backlog: backlog}), do: backlog
 
   defp being_manager(project, socket) do
     if project.manager_id == user(socket).id do
