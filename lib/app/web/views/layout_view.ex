b2405306384d28ca0873c879298e2ff69049fb44
lubien <lubien1996@gmail.com>
2017-08-29 18:39:05 -0300

    Use manifest.json assets on Phoenix

--- lib/app/web/views/layout_view.ex
+++ lib/app/web/views/layout_view.ex
@@ -1,3 +1,9 @@
 defmodule App.Web.LayoutView do
   use App.Web, :view
+
+  @manifest File.read!("priv/static/manifest.json") |> Poison.decode!
+
+  def manifest(key) do
+    @manifest[key]
+  end
 end
